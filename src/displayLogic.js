35f5bcf3c2435d261018ae8ecf1a1586353c8f6d
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-12-27 11:36:56 +0100

    Fx to display one question

--- src/displayLogic.js
+++ src/displayLogic.js
@@ -2,8 +2,28 @@ import { fetchLogic } from "./fetchLogic";
 
 export const displayLogic = () => {
   const quizContainer = document.getElementById("quiz-container");
+  let gameStart = false;
+  let currentQuestionIndex = 0;
+
+  const displayButton = () => {
+    const button = document.createElement("button");
+    button.classList.toggle("game-button");
+    button.textContent = "Start";
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.toggle("button-container");
+    quizContainer.appendChild(buttonContainer);
+    buttonContainer.appendChild(button);
+  };
+
+  const incrementIndex = () => currentQuestionIndex++;
+
+  const setIncrementIndex = () => currentQuestionIndex;
 
   const displayQuiz = async () => {
+    displayButton();
+    gameStart = true;
+
     const apiUrl =
       "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple";
 
@@ -12,12 +32,26 @@ export const displayLogic = () => {
     const results = data.results;
     console.log(results);
 
-    const firstQ = results[0];
-    console.log("first question:", firstQ);
-    const question = document.createElement("p");
-    question.classList.toggle("question");
-    question.textContent = firstQ.question;
-    quizContainer.appendChild(question);
+    quizContainer.addEventListener("click", () => {
+      console.log("game started:", gameStart);
+
+      const nextButton = document.querySelector(".game-button");
+      nextButton.remove();
+
+      const firstQ = results[currentQuestionIndex];
+      console.log("first question:", firstQ);
+
+      const question = document.createElement("p");
+      question.classList.toggle("question");
+      question.textContent = firstQ.question;
+      quizContainer.appendChild(question);
+
+      incrementIndex();
+      setIncrementIndex();
+      console.log("current question index:", currentQuestionIndex);
+
+      displayButton();
+    });
   };
 
   return { displayQuiz };
